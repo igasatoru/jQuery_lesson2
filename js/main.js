76927bb4af01fe959f5de153776ef06460280cee
Satoru Igarashi <igasatoru@gmail.com>
2021-01-08 18:17:28 +0900

    css追加

--- js/main.js
+++ js/main.js
@@ -2,5 +2,17 @@ $(function(){
     $('#typo').typoShadow();
     $('h1').typoShadow();
 
-    $('h1').css('color', '#ebc000');
+    $('h1').css('color', 'red');
+    // $('#typo .inner').css('transform','rotate(10deg)');
+    // $('.page-main > div:nth-child(1) .inner').css('opacity',0.5);
+
+    /*$('#typo').on('mouseover', function(){
+        $('#typo').css('color','#ebc000');
+    });
+    */
+    $('#typo').on('click', function(){
+        $('#typo').css('background-color','#ebc000');
+        $('h1').css('color','yellow');
+    });
+
 });
