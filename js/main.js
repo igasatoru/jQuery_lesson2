526639c5bb12239b0987dfb4ae82b1d17edbe9b4
Satoru Igarashi <igasatoru@gmail.com>
2021-01-09 10:11:59 +0900

    main.js変更

--- js/main.js
+++ js/main.js
@@ -6,13 +6,32 @@ $(function(){
     // $('#typo .inner').css('transform','rotate(10deg)');
     // $('.page-main > div:nth-child(1) .inner').css('opacity',0.5);
 
-    /*$('#typo').on('mouseover', function(){
+    /*
+    $('#typo').on('mouseover', function(){
         $('#typo').css('color','#ebc000');
     });
     */
+    /*
     $('#typo').on('click', function(){
         $('#typo').css('background-color','#ebc000');
         $('h1').css('color','yellow');
     });
-
+    */
+    $('#typo').on('mouseover', function(){
+        $('#typo .inner').animate({
+            // opacity: 0,
+            // fontSize: '0px',
+            color: 'red'
+        },
+        1000
+        );
+    })
+    .on('mouseout', function(){
+        $('#typo .inner').animate({
+            color: 'black'
+        },
+        1000
+        );
+    });
 });
+
